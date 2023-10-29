af877a1a01974576b1defd4ecdd7eea0532143e9
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-10-29 23:01:37 +0700

    create empty retail dataset task

--- dags/retail.py
+++ dags/retail.py
@@ -1,6 +1,7 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -18,6 +19,12 @@ def retail():
         bucket='u1_elt_bucket',
         gcp_conn_id='gcp',
         mime_type='text/csv'
+    )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp'
     )
 
 retail()
