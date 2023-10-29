372905d8d198c52fd491c1b7257412e26a728fb8
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-10-29 22:57:24 +0700

    create upload csv to gcs task

---
+++ dags/retail.py
@@ -0,0 +1,23 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['retail']
+)
+
+def retail():
+    
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_csv_to_gcs',
+        src='/usr/local/airflow/include/dataset/online_retail.csv',
+        dst='raw/online_retail.csv',
+        bucket='u1_elt_bucket',
+        gcp_conn_id='gcp',
+        mime_type='text/csv'
+    )
+
+retail()
