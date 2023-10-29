8e72648dae9c8020eca29af5c6a509e1fe7219af
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-10-29 23:19:53 +0700

    create gcs to bq task

--- dags/retail.py
+++ dags/retail.py
@@ -2,6 +2,10 @@ from airflow.decorators import dag, task
 from datetime import datetime
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -25,6 +29,21 @@ def retail():
         task_id='create_retail_dataset',
         dataset_id='retail',
         gcp_conn_id='gcp'
+    )
+
+    gcs_to_bq = aql.load_file(
+        task_id='gcs_to_bq',
+        input_file=File(
+            'gs://u1_elt_bucket/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False
     )
 
 retail()
